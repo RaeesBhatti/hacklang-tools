cf5a4224928bd010d7c3be6b7dddf91065a349a4
Raees Bhatti <raees.bhatti@gmail.com>
2016-09-26 03:18:43 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# HackLang Tools
+HackLang tools that can be run on any platform including Windows using Docker or WSL
+
+### Installation
+Download `hh_client` binary from the latest release and put in a directory that is accessible by `$PATH`
+
+### Usage:
+Create a `.hhtools` files in the base directory of your project.
+```json
+{
+  "provider": "docker",
+  "image": "hhvm/hhvm:3.12.1"
+}
+```
+You can also specify `wsl` as provider to use Windows Subsystem for Linux. If you have installed HHVM there.
+
+Now you can run `hh_client` against your project and it will 
---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"os/exec"
+	"os"
+	"io"
+	"fmt"
+	"strings"
+	"errors"
+	"path/filepath"
+	"io/ioutil"
+	"encoding/json"
+	"runtime"
+	"crypto/sha1"
+	"encoding/hex"
+	"bufio"
+)
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	conf, err := readConfig(pwd)
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		return
+	}
+
+	cmd := exec.Command("hh_client", os.Args[1:]...)
+
+	if runtime.GOOS == "windows" && conf.Provider == "wsl" {
+		cmd = exec.Command("bash", "-c",
+			`"` + strings.Join(os.Args[1:], " ") + `"`)
+		cmd.Dir = pwd
+	}
+	if conf.Provider == "docker" {
+		containerId, err := getDockerContainerId(conf.RemotePath, conf.Image)
+		if err != nil {
+			panic(err)
+		}
+		cmd = exec.Command("docker", "exec", containerId,
+			"/bin/sh", "-c", `cd "` + conf.RemotePath + `"; hh_client ` +  strings.Join(os.Args[1:], " "))
+	}
+
+	stdout, err := cmd.StdoutPipe();
+	if err != nil {
+		panic(err)
+	}
+	stderr, err := cmd.StderrPipe();
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	defer cmd.Wait()
+
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+
+}
+
+func readConfig(pwd string) (Config, error) {
+	splits := strings.Split(pwd, string(os.PathSeparator))
+
+	var path string
+	var config Config
+
+	for i := 0; i < len(splits); i++ {
+		path = strings.Join(splits[:len(splits)-i], string(os.PathSeparator))
+		if strings.EqualFold(path, ""){
+			path = string(os.PathSeparator)
+		}
+		file := filepath.Join(path, ".hhtools")
+
+		if _, err := os.Stat(file); err == nil {
+
+			dat, err := ioutil.ReadFile(file)
+			if err != nil {
+				panic(err)
+			}
+
+			err = json.Unmarshal(dat, &config)
+			if err != nil {
+				fmt.Println("Invalid JSON at", file, ".")
+				panic(err)
+			}
+
+			if config.verify() != true {
+				return config, errors.New("Invalid configuration detected")
+			}
+			config.LocalPath = filepath.Dir(file)
+			config.RemotePath = determineRemotePath(filepath.Dir(file), config.Provider)
+			return config, nil
+		}
+	}
+
+	return config, errors.New("No .hhtools config file found in the current or any parent directory.")
+}
+
+func determineRemotePath(path string, provider string) string {
+	if runtime.GOOS == "windows" && provider == "docker" {
+		return filepath.ToSlash(
+			strings.Replace(path, filepath.VolumeName(path),
+				`/` + strings.ToLower(strings.Replace(filepath.VolumeName(path), ":", "", 1)),
+				1))
+	}
+	return path
+}
+
+func getDockerContainerId(path string, image string) (string, error) {
+	var toReturn string
+	h := sha1.New()
+	h.Write([]byte(path))
+	hash := hex.EncodeToString(h.Sum(nil))
+
+	tmp := filepath.Join(os.TempDir(), "hhtools_" + hash + ".tmp")
+
+	if _, err := os.Stat(tmp); err == nil {
+		dat, err := ioutil.ReadFile(tmp)
+		if err != nil {
+			panic(err)
+		}
+
+		if len(dat) != 64 {
+			id, err := createDockerContainer(path, image, tmp)
+			if err != nil {
+				return toReturn, err
+			}
+			return id, nil
+		}
+		return string(dat), nil
+	}
+	id, err := createDockerContainer(path, image, tmp)
+	if err != nil {
+		return toReturn, err
+	}
+	return id, nil
+}
+
+func createDockerContainer(path string, image string, tmp string) (string, error) {
+	var toReturn string
+
+	fmt.Println("Starting Docker container. It may take some time if container image has to be downloaded.")
+
+	cmd := exec.Command("docker", "run", "-t", "-d", "-v",  path + ":" + path, image)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return toReturn, err
+	}
+	scanner := bufio.NewScanner(stdout)
+	go func() {
+		for scanner.Scan() {
+			toReturn += scanner.Text()
+		}
+	}()
+	err = cmd.Start()
+	if err != nil {
+		return toReturn, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return toReturn, err
+	}
+
+	splits := strings.Split(toReturn, "\n")
+	toReturn = string(splits[len(splits) - 1])
+
+	err = ioutil.WriteFile(tmp, []byte(toReturn), 0644)
+	if err != nil {
+		return toReturn, err
+	}
+
+	fmt.Println("Docker container has been started successfuly")
+
+	return toReturn, nil
+}
+
+type Config struct {
+	LocalPath	string
+	RemotePath	string
+	Provider	string	`json:"provider"`
+	Image		string	`json:"image"`
+}
+
+func (c Config) verify() bool {
+	if len(c.Provider) < 1 {
+		return false
+	}
+	return true
+}
